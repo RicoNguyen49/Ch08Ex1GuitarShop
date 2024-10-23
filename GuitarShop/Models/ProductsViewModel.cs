babd26a5fd2bc707fd2cbf371c58da1051a4f351
RicoNguyen49 <Nguyen49@mail.nmc.edu>
2024-10-23 17:22:41 -0400

    Add project files.

---
+++ GuitarShop/Models/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using GuitarShop.Models;
+using System.Collections.Generic;
+
+public class ProductsViewModel
+{
+    public List<Category> Categories { get; set; } = null!;
+    public List<Product> Products { get; set; } = null!;
+    public string SelectedCategory { get; set; } = string.Empty;
+
+    public string CheckActiveCategory(string category) =>
+        category == SelectedCategory ? "active" : "";
+}
